a35f13a9a9712ff34c2c773c7fe05269254bff08
SemihBKGR <buyukgungorsemih@gmail.com>
2022-02-16 21:12:10 +0300

    init spring project

---
+++ src/main/java/com/semihbkgr/example/springboot/tale/SpringBootExampleTaleApplication.java
@@ -0,0 +1,13 @@
+package com.semihbkgr.example.springboot.tale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleTaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExampleTaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semihbkgr/example/springboot/tale/SpringBootExampleTaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semihbkgr.example.springboot.tale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleTaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
